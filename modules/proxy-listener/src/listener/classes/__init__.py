4a7ddfa2d322a691bd4d74bd9b3ad777ae3d42dd
IlyasDeckers <ilyas@phasehosting.io>
2017-10-20 12:05:18 +0200

    refactoring

--- modules/proxy-listener/src/listener/classes/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-from cmd import Cmd
-from nginx import Nginx
-from consul import Consul
-from config import Config
-from letsencrypt import Letsencrypt
--- modules/proxy-listener/src/listener/watchers/__init__.py
+++
@@ -1,3 +0,0 @@
-#!/bin/python
-
-from watcher import Watcher
